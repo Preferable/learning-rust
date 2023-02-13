be5f38ce65420b1cb11dd3c6e708e3fb6f54b083
Lilith-v2 <ivyy@duck.com>
2023-02-13 22:27:53 +0100

    Finished Variables

---
+++ src/Variables/Challenge_1.rs
@@ -0,0 +1,12 @@
+fn test() {
+    let mut x = 1000;
+    let y = "Programming";
+
+    println!("x:{}", x);
+    println!("y:{}", y);
+
+    x = 1100;
+    
+    println!("x:{}", x);
+    println!("y:{}", y);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    print!("1\n22\n333\n4444\n55555")
+    println!("Hello, world!");
 }
