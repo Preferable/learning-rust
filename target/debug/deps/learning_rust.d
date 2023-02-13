6673cb59d7a7c9b872f432d5a0e59ae8f3bc8d1f
Lilith-v2 <ivyy@duck.com>
2023-02-13 20:53:20 +0100

    first commit

---
+++ target/debug/deps/learning_rust.d
@@ -0,0 +1,5 @@
+C:\Users\ivyy\Desktop\learnin' rust\learning-rust\target\debug\deps\learning_rust.exe: src\main.rs
+
+C:\Users\ivyy\Desktop\learnin' rust\learning-rust\target\debug\deps\learning_rust.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/learning-rust.d
@@ -0,0 +1,1 @@
+C:\Users\ivyy\Desktop\learnin'\ rust\learning-rust\target\debug\learning-rust.exe: C:\Users\ivyy\Desktop\learnin'\ rust\learning-rust\src\main.rs
